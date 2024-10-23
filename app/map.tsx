523031f824316c84e3810f3969062be3f060bcdb
Abed Rass <abd.assoum@outlook.com>
2024-10-23 10:53:18 +0200

    Implement marker addition on long press in MapScreen; refactor state management and layout using react-native-maps

--- app/map.tsx
+++ app/map.tsx
@@ -1,20 +1,47 @@
-import MapView from "react-native-maps";
-
-
+import MapView, { Marker } from "react-native-maps";
+import { useState } from "react";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
 
 export default function MapScreen() {
+  const [markers, setMarkers] = useState([])
+  const [region, setRegion] = useState({
+    latitude: 55.6704,  
+    longitude: 12.5785, 
+    latitudeDelta: 0.005,  
+    longitudeDelta: 0.005,  
+  });
+
+  function addMarker(data) {
+    const {latitude, longitude} = data.nativeEvent.coordinate
+    const newMarker = { 
+      coordinate: {latitude, longitude}, 
+      key: data.timeStamp,
+      title: "Great Place"
+    }
+
+    setMarkers([...markers, newMarker])
+
+  }
+
   return (
+    <View>
     <MapView
       style={{ flex: 1 }}
-        region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+      region={region}
+      onLongPress={addMarker}
+    >
+
+    {markers.map (marker => (
+      <Marker
+        key={marker.key}
+        coordinate={marker.coordinate}
+        title={marker.title}
+      />
+    ))}
+
+    </MapView>
+    </View>
+
   );
 }
-
-
 
