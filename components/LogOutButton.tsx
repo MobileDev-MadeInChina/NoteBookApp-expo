3d7b898593efe55f269486fd4d90765842b19db9
Jack Hansen <jackvwh@hotmail.com>
2024-10-31 20:46:52 +0100

    Add LogoutButton component: implement logout functionality with error handling and navigation to login page after logging out

---
+++ components/LogOutButton.tsx
@@ -0,0 +1,29 @@
+import { useAuth } from "@/app/AuthContext";
+import { useRouter } from "expo-router";
+import { Alert, Text, TouchableOpacity } from "react-native";
+
+export default function LogoutButton() {
+  const { user, logout } = useAuth();
+  const router = useRouter();
+
+  async function handleLogout() {
+    try {
+      await logout();
+      router.replace("/login");
+    } catch (error) {
+      console.error("Error signing out: ", error);
+      Alert.alert("Error", "Failed to sign out. Please try again.");
+    }
+  }
+
+  return (
+    <TouchableOpacity
+      className="bg-red-500 py-3 rounded-md"
+      onPress={handleLogout}>
+      <Text className="text-white text-center font-bold">
+        {" "}
+        Logout: {user?.email}
+      </Text>
+    </TouchableOpacity>
+  );
+}
