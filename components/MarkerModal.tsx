860fbec1bd640110d6a52893966b7bdef9639246
Jack <jackvwh@hotmail.com>
2025-01-04 19:52:11 +0100

    Update dependencies in MarkerModal useEffect to include the entire marker object

--- components/MarkerModal.tsx
+++ components/MarkerModal.tsx
@@ -96,7 +96,7 @@ export function MarkerModal({
     }
 
     fetchNote();
-  }, [marker.key, user]);
+  }, [marker, user]);
 
   // Handle image picker functionality
   async function handleImagePicker() {
